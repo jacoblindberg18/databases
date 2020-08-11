7192581bdd7ea5cd485fb4240ab4d97368ecc288
jacoblindberg18 <jacob.lindberg18@gmail.com>
2020-08-11 16:44:52 +0200

    added code in app.js

--- FilmAPI/app.js
+++ FilmAPI/app.js
@@ -1,1 +1,16 @@
-const jacob = "jacob";
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+app.options("*", cors());
+
+app.server = app.listen(port, () => {
+    console.log(`Runningonport${port}`);
+});
+
+module.exports = app;
